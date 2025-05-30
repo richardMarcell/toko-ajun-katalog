e3bc0f7502b26fd12f789fb9a5c5f044fddb0e88
Richard Marcell <richard.marcell8888@gmail.com>
2025-05-30 09:27:57 +0700

    feat: implement command to refresh database

---
+++ db/reset-database.ts
@@ -0,0 +1,25 @@
+import { sql } from "drizzle-orm";
+import { db } from "@/db";
+
+async function resetDatabase() {
+  await db.execute(sql`SET FOREIGN_KEY_CHECKS = 0`);
+
+  /* eslint-disable @typescript-eslint/no-explicit-any */
+  const tables = (await db.execute(
+    sql`SELECT table_name FROM information_schema.tables WHERE table_schema = DATABASE()`,
+  )) as any[];
+
+  for (const row of tables[0]) {
+    const tableName = row.TABLE_NAME;
+    await db.execute(sql.raw(`TRUNCATE TABLE \`${tableName}\``));
+  }
+
+  await db.execute(sql`SET FOREIGN_KEY_CHECKS = 1`);
+  console.log("✅ All tables truncated");
+  process.exit(0);
+}
+
+resetDatabase().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
